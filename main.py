86ccc68e313ee2551dda1d6a46b5a47d1fc46996
Agung Cantona <72956597+agungcantona@users.noreply.github.com>
2022-05-26 15:45:00 +0800

    adding POST request
    adding POST request and creating  some value to receive the data

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import os
 import json
 import random
 import urllib.request
-
 from flask import Flask, redirect, render_template, request
-
 from google.cloud import datastore
 from google.cloud import storage
 from google.cloud import vision
@@ -16,16 +14,93 @@ CLOUD_STORAGE_BUCKET = os.environ.get("CLOUD_STORAGE_BUCKET")
 
 
 app = Flask(__name__)
-link_affiliation = 'https://insul-in-default-rtdb.firebaseio.com/affiliation_product.json'
-link_article = 'https://insul-in-default-rtdb.firebaseio.com/article.json'
 
-@app.route("/")
+#saving links for affiliation & article
+link_affiliation = 'https://insul-in-default-rtdb.firebaseio.com/affiliation_product.json?limitToLast=3&orderBy=%22$key%22'
+link_article = 'https://insul-in-default-rtdb.firebaseio.com/article.json?limitToLast=3&orderBy=%22$key%22'
+
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
+
+    # if request POST
+    if request.method == 'POST':
+
+        # parse variable
+        age = request.form.get('age')
+        gender = request.form.get('gender')
+        polyuria = request.form.get('polyuria')
+        polydipsia = request.form.get('polydipsia')
+        weightLoss = request.form.get('weightLoss')
+        weakness = request.form.get('weightness')
+        polyphagia = request.form.get('polyphagia')
+        genital_thrus = request.form.get('genital_thrus')
+        itching = request.form.get('itching')
+        irritability = request.form.get('irritability')
+        delayed_healing = request.form.get('delayed_healing')
+        partial_paresis = request.form.get('partial_paresis')
+        muscle_stiffness = request.form.get('muscle_stiffness')
+        alopecia = request.form.get('alopecia')
+        obesity = request.form.get('obesity')
+
+        #inputing data to an object called userData
+        userData ={}
+        userData["age"] = age
+        userData["gender"] = gender
+        userData["polyuria"] = polyuria
+        userData["polydipsia"] = polydipsia
+        userData["weightLoss"] = weightLoss
+        userData["weakness"] = weakness
+        userData["polyphagia"] = polyphagia
+        userData["genital_thrus"] = genital_thrus
+        userData["itching"] = itching
+        userData["irritability"] = irritability
+        userData["delayed_healing"] = delayed_healing
+        userData["partial_paresis"] = partial_paresis
+        userData["muscle_stiffness"] = muscle_stiffness
+        userData["alopecia"] = alopecia
+        userData["obesity"] = obesity
+
+        json_user = json.dumps(userData)
+        json_loadUser= json.loads(json_user)
+
+        if json_loadUser["result_diagnose"] == False:
+
+            #reading data from url
+            with urllib.request.urlopen(link_article) as url:
+                article = json.loads(url.read().decode())
+
+                #Creating affiliation list
+                x = {}
+                x["article"] = article
+
+                #merge result
+                merged_result = { **json_loadUser, **x}
+                result= json.dumps(merged_result)
+                final_result= json.loads(result)
+                return final_result
+
+        elif json_loadUser["result_diagnose"] == True:
+
+            #reading data from url
+            with urllib.request.urlopen(link_affiliation) as url:
+                affiliation = json.loads(url.read().decode())
+
+                #Creating affiliation list
+                x = {}
+                x["affiliation_product"] = affiliation
+
+                #merge result
+                merged_result = { **json_loadUser, **x}
+                result= json.dumps(merged_result)
+                final_result= json.loads(result)
+                return final_result
+    
+    #if not POST
     userData ={
-        "result_diagnose":bool(random.getrandbits(1)),
-        "error":bool(0),
-        "message":"success"
-    }
+            "result_diagnose": bool(random.getrandbits(1)),
+            "error": bool(0),
+            "message": "success"
+        }
 
     json_user = json.dumps(userData)
     json_loadUser= json.loads(json_user)
@@ -35,10 +110,12 @@ def homepage():
         #reading data from url
         with urllib.request.urlopen(link_article) as url:
             article = json.loads(url.read().decode())
-            i = list(range(3))
-            article_list = [article[0], article[1], article[2], article[3], article[4]]
+
+            #Creating affiliation list
             x = {}
-            x["article"] = random.sample(article_list, len(i))
+            x["article"] = article
+
+            #merge result
             merged_result = { **json_loadUser, **x}
             result= json.dumps(merged_result)
             final_result= json.loads(result)
@@ -49,15 +126,19 @@ def homepage():
         #reading data from url
         with urllib.request.urlopen(link_affiliation) as url:
             affiliation = json.loads(url.read().decode())
-            i = list(range(3))
-            affiliation_list = [affiliation[0], affiliation[1], affiliation[2], affiliation[3], affiliation[4], affiliation[5]]
+
+            #Creating affiliation list
             x = {}
-            x["affiliation_product"] = random.sample(affiliation_list, len(i))
+            x["affiliation_product"] = affiliation
+
+            #merge result
             merged_result = { **json_loadUser, **x}
             result= json.dumps(merged_result)
             final_result= json.loads(result)
             return final_result
 
+
+# If server error
 @app.errorhandler(500)
 def server_error(e):
     logging.exception("An error occurred during a request.")
@@ -72,6 +153,4 @@ def server_error(e):
     )
 
 if __name__ == "__main__":
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host="127.0.0.1", port=8080, debug=True)
